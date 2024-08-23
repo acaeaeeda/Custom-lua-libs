709c542838925a9798fa101fc4a5d1c9c6082587
acaeaeeda <147932550+acaeaeeda@users.noreply.github.com>
2024-08-23 13:59:00 +0800

    添加文件
    添加文件

---
+++ famerUtils/dataManager.lua
@@ -0,0 +1,15 @@
+-- Create table.
+local crops = {}
+-- Mod id fields.
+crops.minecraft = {}
+crops.farmersdelight = {}
+
+-- Specified block id.
+-- e.g.
+--   mod id    block id   age item id
+crops.minecraft.carrots = {7,"carrot"}
+
+
+
+--Return.
+return crops
--- famerUtils/famerutils.lua
+++ famerUtils/famerutils.lua
@@ -1,1 +1,36 @@
+local crops = require("dataManager")
+
+
+
+local function nameSpliter(text)
+    local marker = string.find(text,":")
+    local modid = string.sub(text,1,marker-1)
+    local name = string.sub(text,marker+1,string.len(text))
+    return modid,name
+end
+
+
+local function canReap()
+    local k,block = turtle.inspectDown()
+    if k then
+        local modid,name = nameSpliter(block.name)
+        if crops[modid] then
+            if crops[modid][name] then
+                return crops[modid][name][0] == block.state.age
+            end
+        end
+    end
+                    
+end
+
+
+
+-- test
+print(canReap())
+
+
+-- Return
+return {
+    canReap = canReap
+}
 
