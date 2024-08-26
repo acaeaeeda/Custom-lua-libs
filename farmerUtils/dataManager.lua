ae908019403f943ec8ac7c350395b2a574457d8d
acaeaeeda <147932550+acaeaeeda@users.noreply.github.com>
2024-08-26 08:38:32 +0800

    Update dataManager.lua

--- farmerUtils/dataManager.lua
+++ farmerUtils/dataManager.lua
@@ -6,13 +6,9 @@
 ]]
 -- Create table.
 local crops = {}
--- Mod id fields.
-crops.minecraft = {}
-crops.farmersdelight = {}
 
--- Specified block id.
 -- e.g.
---   mod id    block id   age item id
+crops.minecraft = {}
 crops.minecraft.carrots = {7,"carrot"}
 
 
